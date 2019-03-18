d0a164fbb01a10c05be1c8f4559431f7f57dd05f
wangchao <wcd126yx@126.com>
2019-03-18 14:14:20 +0800

    最大子序和

--- 53.最大子序和.js
+++ 53.最大子序和.js
@@ -29,7 +29,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-    
+var maxSubArray = function (nums) {
+    if (nums.length <= 1) return nums[0]
+    let sum = nums[0]
+    let MAX = sum
+    for (let i = 1; i < nums.length; i++) {
+        if (sum >= 0) {
+            if (sum + nums[i] >= MAX) {
+                MAX = sum + nums[i]
+            }
+            sum = sum + nums[i]
+        } else {
+            if (nums[i] >= MAX) {
+                MAX = nums[i]
+            }
+            sum = nums[i]
+        }
+    }
+    return MAX
 };
 
