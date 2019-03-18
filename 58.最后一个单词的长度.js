3165923ff036b95701485453f24aff189d5d6362
wangchao <wcd126yx@126.com>
2019-03-18 14:51:01 +0800

    58-最后一个单词的长度

---
+++ 58.最后一个单词的长度.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=javascript
+ *
+ * [58] 最后一个单词的长度
+ *
+ * https://leetcode-cn.com/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (28.87%)
+ * Total Accepted:    19.8K
+ * Total Submissions: 68.5K
+ * Testcase Example:  '"Hello World"'
+ *
+ * 给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+ * 
+ * 如果不存在最后一个单词，请返回 0 。
+ * 
+ * 说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+ * 
+ * 示例:
+ * 
+ * 输入: "Hello World"
+ * 输出: 5
+ * 
+ * 
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    // 去掉首位空格，再分割字符串
+    var nums = s.trim().split(' ');
+    return nums[nums.length - 1].length
+};
+
