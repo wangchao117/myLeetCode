b9b39a7cfd41fb02df29ddcd2dfcec7c432e05d0
wangchao <wcd126yx@126.com>
2019-05-27 14:07:18 +0800

    190.颠倒二进制

---
+++ 190.颠倒二进制位.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=190 lang=javascript
+ *
+ * [190] 颠倒二进制位
+ */
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function (n) {
+    let res = 0;
+    for (let i = 0; i < 32; i++) {
+        res = (res << 1) + (n & 1);
+        n = n >>> 1;
+    }
+
+    return res >>> 0;
+};
+
